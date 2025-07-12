c3efec1f193581b76b48e1f69efe42f71ac2278e
cag185 <cag185@pitt.edu>
2025-07-12 01:28:21 -0400

    fixed repo base in vite config

--- National-Park-Web/vite.config.ts
+++ National-Park-Web/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "National-Park-Web",
+  base: "/National-Park-Web/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
