c4d651a76d0feb26b47abbaf81425b6c0da33f1f
cag185 <cag185@pitt.edu>
2025-07-12 01:58:37 -0400

    fixed imports

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+// export default router
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue'
+import MapView from '../views/MapView.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView,
+    },
+    {
+      path: '/maps',
+      name: 'Map',
+      component: MapView,
+    }
+  ],
+});
+
+export default router;
