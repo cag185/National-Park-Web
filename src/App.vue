a522bf5aa374576cb7f50774fb41c3c59b58fcee
cag185 <cag185@pitt.edu>
2025-07-22 17:05:56 -0400

    created a nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import RouterComponent from "./components/RouterComponent.vue";
+</script>
+
 <template>
+  <RouterComponent />
   <RouterView />
 </template>
---
+++ src/components/RouterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-row justify-start items-center space-x-4 p-4">
+    <div class="flex space-y-2" v-for="page in pageNames" :key="page.name">
+      <router-link
+        :to="{ name: page.name }"
+        class="text-white text-3xl hover:text-blue-200"
+      >
+        {{ page.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const routes = router.getRoutes();
+
+const pageNames = routes;
+
+onMounted(() => {
+  console.log("Available routes:", routes);
+});
+</script>
